b9848df60d44e48af9306b9120ea143c0bade464
BAMBA928 <bambapartou42@gmail.com>
2026-06-28 02:38:04 +0000

    modification3 du fichier

--- src/services.php
+++ src/services.php
@@ -1,18 +1,82 @@
 <?php
-function creerWallet($Numero, $Nom, $CodeSecret, $Solde) 
+function creerWallet($Numero, $Nom, $CodeSecret, $Solde)
 {
     global $wallets;
     $wallet = ['Numeros' => $Numero, 'client' => $Nom, 'code' => $CodeSecret, 'solde' => $Solde];
-    
+
     enregistrezWallet($wallets, $wallet);
-    
+
 }
-function creerDepot($montant, $Numero) 
+function creerDepot($montant, $Numero)
 {
-  
     global $transactions;
-    $transaction = ['montant' => $montant, 'numero' => $Numero];
+
+    $resultat = modifierSolde($Numero, $montant);
+
+    if ($resultat === false) {
+        echo "Wallet introuvable\n";
+        return;
+    }
+
+    $transaction = [
+        'type' => 'DEPOT',
+        'montant' => $montant,
+        'numero' => $Numero,
+    ];
+
     enregistrezTransaction($transactions, $transaction);
-    
+}
+function creerRetrait($montant, $Numero)
+{
+    global $transactions;
+
+    $wallet = chercherWalletParNumero($Numero);
+
+    if ($wallet === null) {
+        echo "Wallet introuvable\n";
+        return;
+    }
+
+    $frais =calculFrais($montant);
+
+    $totalRetrait=$montant + $frais;
+
+    if ($totalRetrait > $wallet['solde']) {
+        echo "Solde insuffisant\n";
+        return;
+    }
+
+    modifierSolde($Numero, -$totalRetrait);
+
+
+    $transaction = [
+        'type' => 'RETRAIT',
+        'montant' => $montant,
+        'frais' => $frais,
+        'numero' => $Numero,
+    ];
+
+    enregistrezTransaction($transactions, $transaction);
+}
+function calculFrais($montant)
+{
+    if ($montant <= 10000) {
+        return 200;
+    } else if ($montant <= 100000) {
+        return 500;
+    } else {
+        $frais = $montant  / 100;
+        if ($frais > 5000) {
+            return 5000;
+        }
+
+        return $frais;
+    }
+
+}
+
+function listerTransactions()
+{
+    return recupererTransactions();
 }
 ?>
